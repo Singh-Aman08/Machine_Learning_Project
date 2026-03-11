3f130c35ed729fb47effd8915ae0eac0bd9d6785
Aman-Singh075 <aman.singh.2025@bristol.ac.uk>
2026-03-11 20:38:30 +0000

    data ingestion

---
+++ src/data_ingestion.py
@@ -0,0 +1,57 @@
+import sys
+import os 
+from rootlogger import logger
+from exception import CustomException
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+from data_transformation import DataTransformation
+
+@dataclass
+class DataIngestionCongig:
+    train_data_path: str = os.path.join("artifacts", "train.csv")
+    test_data_path: str = os.path.join("artifacts", "test.csv")
+    raw_data_path: str = os.path.join("artifacts", "data.csv")
+    
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionCongig()
+        
+    def initiate_data_ingestion(self):
+        logger.info("Entered the data ingestion method")
+        try:
+            df = pd.read_csv(r"C:\Users\Aman Kumar Singh\Desktop\ML_Project\Machine_Learning_Project\data\cleaned_data.csv")
+            logger.info("Completed data reading")
+            
+            os.makedirs("artifacts", exist_ok=True)
+            
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
+            
+            train_set.to_csv(self.ingestion_config.train_data_path, index = False, header = True)
+            test_set.to_csv(self.ingestion_config.test_data_path, index = False, header = True)
+            df.to_csv(self.ingestion_config.raw_data_path, index = False, header=True)
+            
+            logger.info("Ingestion of the data is completed")
+            
+            return self.ingestion_config.train_data_path, self.ingestion_config.test_data_path
+            
+        except Exception as e:
+            logger.error(f"Data ingestion failed : {e}")
+            raise CustomException(e, sys)
+
+
+
+
+if __name__ == "__main__":
+    obj = DataIngestion()
+    train_path, test_path = obj.initiate_data_ingestion()
+    obj1 = DataTransformation()
+    obj1.initiate_data_transformation(train_path, test_path)
+    
+    
+        
+        
+        
+        
+        
+    
